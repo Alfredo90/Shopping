4be6228ab2b2bc6e1ba523f543f09093f6ab2f14
AlfredoM90 <119818983+AlfredoM90@users.noreply.github.com>
2023-11-02 11:29:31 -0500

    styled Shop By Category file

--- shopping/src/components/ShopByCategory/ShopByCategory.jsx
+++ shopping/src/components/ShopByCategory/ShopByCategory.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
 const ShopByCategory = ({ products }) => {
-  console.log(products)
+  const categories = [
+    { id: 0, category: "Men" },
+    { id: 1, category: "Women" },
+    { id: 2, category: "Jewelery" },
+    { id: 3, category: "Electronic" },
+  ];
+
+  console.log(products);
   return (
-    <div className="flex flex-row justify-center items-center border-2 h-screen w-screen ">
-      <div>
-        <h1 className="text-2xl">Shop By Category</h1>
-      </div>
-      <div>
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>{products.category}</li>
-          ))}
-        </ul>
-      </div>
+    <div className="flex flex-col justify-evenly items-center border-2 h-screen w-screen ">
+      <hr className="border-2 w-5/6 " />
+      <h1 className="text-2xl">Shop By Category</h1>
+      <ul className="  flex flex-row gap-20 ">
+        {categories.map(({ category, id }) => (
+          <li
+            key={id}
+            className="border-2 w-24 h-24 flex justify-center items-center  "
+          >
+            {category}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
